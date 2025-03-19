92ca5dc13bbd95c3810cb5fdfa0085ac47927bbe
Markus Seeli <markus@seeli.eu>
2025-03-19 16:40:54 +0000

    Working version

--- example.py
+++ example.py
@@ -40,5 +40,33 @@ def _():
     return DATABASE_URL, create_engine, engine, os
 
 
+@app.cell
+def _(engine, mo, trade):
+    dftrades = mo.sql(
+        f"""
+        SELECT * FROM trade
+        """,
+        engine=engine
+    )
+    return (dftrades,)
+
+
+@app.cell
+def _(alt, dftrades):
+    def create_chart1(df):  
+
+        # Create chart
+        chart = alt.Chart(df).mark_bar().encode(
+            alt.X('gap_in_atr:Q', bin=True),
+            alt.Y('median(profit_original)')
+           # color='trade_type:N',
+           # tooltip=['symbol', 'trade_date', 'trade_type']
+        ).interactive()
+        return chart
+
+    create_chart1(dftrades)
+    return (create_chart1,)
+
+
 if __name__ == "__main__":
     app.run()
