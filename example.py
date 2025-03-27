30b02776072e2498789a8e38c980312e0f47da6b
Markus Seeli <markus@seeli.eu>
2025-03-27 08:47:26 +0000

    Added more examples

--- example.py
+++ example.py
@@ -9,26 +9,10 @@ def _():
     import marimo as mo
     import altair as alt
     import pandas as pd
+    import polars as pl
 
     alt.renderers.enable('mimetype')
-    return alt, mo, pd
-
-
-@app.cell
-def _(alt, pd):
-    def create_chart():  
-        # Sample data
-        df = pd.DataFrame({'x': range(10), 'y': [v**2 for v in range(10)]})
-
-        # Create chart
-        chart = alt.Chart(df).mark_line().encode(
-            x='x',
-            y='y'
-        )
-        return chart
-
-    create_chart()
-    return (create_chart,)
+    return alt, mo, pd, pl
 
 
 @app.cell
@@ -42,37 +26,42 @@ def _():
 
 @app.cell
 def _(engine, mo, trade):
-    df_gap = mo.sql(
+    df_trades = mo.sql(
         f"""
-        SELECT profit_original, gap_in_atr 
-        FROM trade
+        SELECT * FROM trade
         """,
         engine=engine
     )
-    return (df_gap,)
+    return (df_trades,)
 
 
 @app.cell
-def _(alt, df_gap):
-    def create_gap_scatter(df):
-        scatter = alt.Chart(df).mark_circle().encode(
-            x=alt.X('gap_in_atr:Q', title="Gap in ATR"),
-            y=alt.Y('profit_original:Q', title="Profit Original")
-        )
-        regression = scatter.transform_regression(
-            'gap_in_atr', 
-            'profit_original'
-        ).mark_line(color='red')
-        return (scatter + regression).interactive()
+def _(alt, df_trades):
+    scatter = alt.Chart(df_trades).mark_circle().transform_calculate(
+       side = "datum.trade_type > 0 ? 'Long' :'Short'"
+    ).encode(
+       alt.X('gap_in_atr:Q', title="Gap in ATR"),
+       alt.Y('profit_original:Q', title="Profit Original"),
+       tooltip=['symbol', 'trade_date', 'profit_original'],
+       color=alt.Color('side:N',
+               scale=alt.Scale(
+               domain=['Short', 'Long'],
+               range=['red', 'green']
+            )
+       ),
+   
+    ).properties(
+        width=700
+    )
 
-    create_gap_scatter(df_gap)
-    return (create_gap_scatter,)
+    scatter
+    return (scatter,)
 
 
 @app.cell
-def _(alt, df_gap):
-    def create_gap_barchart(df):
-        chart = alt.Chart(df).mark_bar().encode(
+def _(alt, df_trades):
+
+    chart = alt.Chart(df_trades).mark_bar().encode(
             x=alt.X('gap_in_atr:Q', 
                    bin=alt.Bin(maxbins=10), 
                    title="Gap in ATR (Binned)"),
@@ -84,38 +73,38 @@ def _(alt, df_gap):
                 alt.Tooltip('mean(profit_original):Q', title="Avg Profit", format='.2f')
             ]
         ).interactive()
-        return chart
-
-    create_gap_barchart(df_gap)
-    return (create_gap_barchart,)
+    chart
 
 
-@app.cell
-def _(engine, mo, trade):
-    dftrades = mo.sql(
-        f"""
-        SELECT * FROM trade
-        """,
-        engine=engine
-    )
-    return (dftrades,)
+    return (chart,)
 
 
 @app.cell
-def _(alt, dftrades):
-    def create_chart1(df):  
+def _(df_trades, pl):
 
-        # Create chart
-        chart = alt.Chart(df).mark_bar().encode(
-            alt.X('gap_in_atr:Q', bin=True),
-            alt.Y('median(profit_original)')
-           # color='trade_type:N',
-           # tooltip=['symbol', 'trade_date', 'trade_type']
-        ).interactive()
-        return chart
+    df_gap = df_trades.sort('gap_in_atr')
+    df_gap = df_gap.with_columns(pl.arange(1, df_gap.height + 1).alias("Line_Number"))
+    df_gap = df_gap.with_columns((pl.col('Line_Number') / 1).floor().alias('Bin'))
+                           
+    df_gap
+    return (df_gap,)
 
-    create_chart1(dftrades)
-    return (create_chart1,)
+
+@app.cell
+def _(alt, df_gap):
+    # Create chart
+
+    chart1 = alt.Chart(df_gap).mark_bar().encode(
+        alt.X('Bin:N',  bin=alt.Bin(maxbins=80)),
+        y=('mean(profit_original)'),
+        tooltip=['mean(profit_original)', 'median(profit_original)', 'count()', 'min(gap_in_atr)',  'max(gap_in_atr)' ]
+       # color='gap_in_atr:Q',
+       # [alt.Tooltip('gap_in_atr:Q', title="Bin Start", bin='origin')]
+    ).interactive().properties(title='Average Profit per Gap in ATR',width=700)
+
+
+    chart1
+    return (chart1,)
 
 
 if __name__ == "__main__":
